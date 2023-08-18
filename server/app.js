7467cc93e0cd15e3b8378044af3e312bf31bb165
Dan <dvwhite1981@gmail.com>
2023-08-18 10:48:14 -0400

    Complete

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 // Your code here
+const path = require('path');
+
+// Part 1
+// app.use(express.static('assets'));
+
+// Part 2
+// app.use('/scripts', express.static('assets/scripts'));
+
+// Part 3
+app.use('/stylesheets', express.static('assets/css'));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
